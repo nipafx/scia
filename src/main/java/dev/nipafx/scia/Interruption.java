bd8a2b2d05aa8fe859cad128d21098224d8dff9d
Nicolai Parlog <nicolai@nipafx.dev>
2025-10-09 15:03:07 +0200

    In some places, use `compute` instead of `computeOrRollback` to show states

--- src/main/java/dev/nipafx/scia/Interruption.java
+++ src/main/java/dev/nipafx/scia/Interruption.java
@@ -27,9 +27,9 @@ class Interruption {
 			var taskC = new Task("C");
 
 			try (var scope = StructuredTaskScope.open()) {
-				var subtaskA = scope.fork(() -> taskA.computeOrRollBack(Behavior.run(100)));
-				var subtaskB = scope.fork(() -> taskB.computeOrRollBack(Behavior.fail(200)));
-				var subtaskC = scope.fork(() -> taskC.computeOrRollBack(Behavior.run(1_000)));
+				var subtaskA = scope.fork(() -> taskA.compute(Behavior.run(100)));
+				var subtaskB = scope.fork(() -> taskB.compute(Behavior.fail(200)));
+				var subtaskC = scope.fork(() -> taskC.compute(Behavior.run(1_000)));
 
 				try {
 					scope.join();
@@ -53,7 +53,7 @@ class Interruption {
 			var taskC = new Task("C (inner)");
 
 			try (var scope = StructuredTaskScope.open()) {
-				var subtask = scope.fork(() -> taskA.computeOrRollBack(Behavior.run(1_000)));
+				var subtask = scope.fork(() -> taskA.compute(Behavior.run(1_000)));
 				var subtasks = scope.fork(() -> inner(taskB, taskC));
 
 				try {
@@ -69,8 +69,8 @@ class Interruption {
 
 		String inner(Task task1, Task task2) throws InterruptedException {
 			try (var scope = StructuredTaskScope.open()) {
-				var subtaskB = scope.fork(() -> task1.computeOrRollBack(Behavior.run(1_000)));
-				var subtaskC = scope.fork(() -> task2.computeOrRollBack(Behavior.run(1_000)));
+				var subtaskB = scope.fork(() -> task1.compute(Behavior.run(1_000)));
+				var subtaskC = scope.fork(() -> task2.compute(Behavior.run(1_000)));
 
 				scope.join();
 
@@ -89,9 +89,9 @@ class Interruption {
 			var taskC = new Task("C");
 
 			try (var scope = StructuredTaskScope.open()) {
-				var subtaskA = scope.fork(() -> taskA.computeOrRollBack(Behavior.run(100)));
-				var subtaskB = scope.fork(() -> taskB.computeOrRollBack(Behavior.fail(200)));
-				var subtaskC = scope.fork(() -> taskC.computeOrRollBack(Behavior.runBusy(1_000)));
+				var subtaskA = scope.fork(() -> taskA.compute(Behavior.run(100)));
+				var subtaskB = scope.fork(() -> taskB.compute(Behavior.fail(200)));
+				var subtaskC = scope.fork(() -> taskC.compute(Behavior.runBusy(1_000)));
 
 				try {
 					scope.join();
@@ -118,9 +118,9 @@ class Interruption {
 					StructuredTaskScope.Joiner.awaitAllSuccessfulOrThrow(),
 					config -> config.withTimeout(Duration.ofMillis(500))
 			)) {
-				var subtaskA = scope.fork(() -> taskA.computeOrRollBack(Behavior.run(100)));
-				var subtaskB = scope.fork(() -> taskB.computeOrRollBack(Behavior.run(800)));
-				var subtaskC = scope.fork(() -> taskC.computeOrRollBack(Behavior.runBusy(1_000)));
+				var subtaskA = scope.fork(() -> taskA.compute(Behavior.run(100)));
+				var subtaskB = scope.fork(() -> taskB.compute(Behavior.run(800)));
+				var subtaskC = scope.fork(() -> taskC.compute(Behavior.runBusy(1_000)));
 
 				try {
 					scope.join();
@@ -153,9 +153,9 @@ class Interruption {
 							.withName("important scope 🚀")
 					// .withThreadFactory()
 			)) {
-				var subtaskA = scope.fork(() -> taskA.computeOrRollBack(Behavior.run(100)));
-				var subtaskB = scope.fork(() -> taskB.computeOrRollBack(Behavior.run(400)));
-				var subtaskC = scope.fork(() -> taskC.computeOrRollBack(Behavior.runBusy(1_000)));
+				var subtaskA = scope.fork(() -> taskA.compute(Behavior.run(100)));
+				var subtaskB = scope.fork(() -> taskB.compute(Behavior.run(400)));
+				var subtaskC = scope.fork(() -> taskC.compute(Behavior.runBusy(1_000)));
 
 				ThreadDumper.createDumpAfter(0);
 
