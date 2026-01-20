7f8f6c16ca4e232470d44e77cac0a00d41a51064
Nicolai Parlog <nicolai@nipafx.dev>
2026-01-20 23:04:05 +0100

    Fix a typo

--- src/main/java/dev/nipafx/scia/queue/package-info.java
+++ src/main/java/dev/nipafx/scia/queue/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Even more than the other code in this demo project, this package is NOT FIR FOR PRODUCTION.
+ * Even more than the other code in this demo project, this package is NOT FOR PRODUCTION.
  * The queues are stand-ins for proper concurrent data structures.
  */
 package dev.nipafx.scia.queue;
